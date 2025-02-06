66825de9284df1a014c2e85703327f9f80da0395
Michael Miller <mjm9189@gmail.com>
2025-02-06 02:15:51 -0800

    first commit

---
+++ src/Cell.java
@@ -0,0 +1,32 @@
+public class Cell {
+
+    enum CellType {
+        EMPTY,
+        FOOD,
+        SNAKE
+    }
+
+    private final int row, col;
+    private CellType cellType;
+
+    public Cell(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public getCellType() {
+        return this.cellType;
+    }
+
+    public setCellType(CellType cellType) {
+        this.cellType = cellType;
+    }
+
+    public int getRow() {
+        return self.row;
+    }
+
+    public int getCol() {
+        return self.col;
+    }
+}
