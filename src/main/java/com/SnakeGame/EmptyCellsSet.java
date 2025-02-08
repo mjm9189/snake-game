e29116158693b5b618ea849961c94e7a3d0daf65
Michael Miller <mjm9189@gmail.com>
2025-02-08 03:26:07 -0800

    create method to track empty cells for food generation

---
+++ src/main/java/com/SnakeGame/EmptyCellsSet.java
@@ -0,0 +1,29 @@
+package com.SnakeGame;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class EmptyCellsSet {
+    private ArrayList<Cell> emptyCells;
+
+    public EmptyCellsSet(ArrayList<Cell> emptyCells) {
+        this.emptyCells = emptyCells;
+    }
+    
+    public void replaceCell(Cell outCell, Cell inCell) {
+        int index = outCell.getEmptyArrayIndex();
+        outCell.setEmptyArrayIndex(5000);
+        this.emptyCells.add(index, inCell);
+        inCell.setEmptyArrayIndex(index);
+    }
+
+    public void removeCell(Cell cell) {
+        Cell last = this.emptyCells.get(this.emptyCells.size() - 1);
+        this.replaceCell(cell, last);
+    }
+
+    public Cell getRandomCell() {
+        Random rand = new Random();
+        return this.emptyCells.get(rand.nextInt(this.emptyCells.size()));
+    }
+}
